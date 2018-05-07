7eb4586fc4119af6a7a8bc2eca48febb1844eb1e
Mateo <MateoWartelle@gmail.com>
2018-05-07 12:06:33 -0500

    + Added Remotely_End_Any_Process
    + Powershell script to end a process name on a specific computer on your domain.

---
+++ Remotely_End_Any_Process.ps1
@@ -0,0 +1,24 @@
+﻿$ComputerName = Read-Host -Prompt 'Input Computer name'
+$ProcessName = Read-Host -Prompt 'Input Process Name'
+
+$Processes = Get-WmiObject -Class Win32_Process -ComputerName $ComputerName -Filter "name='$ProcessName'" -ErrorAction SilentlyContinue
+
+foreach ($process in $processes) {
+try{
+  $returnval = $process.terminate()
+  $processid = $process.handle
+  }
+ catch {
+}
+Finally {
+}
+ 
+if($returnval.returnvalue -eq 0) {
+  write-host "The process $ProcessName `($processid`) terminated successfully"
+}
+else {
+  write-host "The process $ProcessName `($processid`) termination has some problems"
+}
+
+}
+
